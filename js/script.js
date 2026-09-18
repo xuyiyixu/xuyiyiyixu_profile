43261e346a3040a4b9e8843f3370146ad76852a4
yiyiyixu <xiaodouhuang@yiyiyixu.mynetworksettings.com>
2026-09-18 12:54:49 -0400

    add linkedin

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('internships-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+if (document.getElementById('contact-link')) {
+  document.getElementById('contact-link').addEventListener('click', (event) => {
+    event.preventDefault()
+    document.getElementById('footer').scrollIntoView({behavior: "smooth"})
+  })
+}
